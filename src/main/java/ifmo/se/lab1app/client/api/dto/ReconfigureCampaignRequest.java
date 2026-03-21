d146fb2b9b27286bf05d15e17add55421c0ee1d4
ren1kron <aleksy.pro35@gmail.com>
2026-03-21 11:12:38 +0300

    HOTFIX: fix last validation issue

--- src/main/java/ifmo/se/lab1app/client/api/dto/ReconfigureCampaignRequest.java
+++ src/main/java/ifmo/se/lab1app/client/api/dto/ReconfigureCampaignRequest.java
@@ -1,9 +1,6 @@
 package ifmo.se.lab1app.client.api.dto;
 
-import jakarta.validation.constraints.Digits;
-import jakarta.validation.constraints.Future;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -11,5 +8,5 @@ import java.time.LocalDateTime;
 public record ReconfigureCampaignRequest(
         @Positive @Digits(integer = 13, fraction = 2) BigDecimal budgetAmount,
         @Future LocalDateTime requestedStartAt,
-        @Positive @Size(max = 365) Integer durationDays
+        @Positive @Max(365) Integer durationDays
 ) {}
